1107b825e6ac44290ca715618104ad5ef73e2f08
muhmadjaradat <you@example.com>
2021-03-04 16:54:46 +0200

    g

---
+++ JAVA.js
@@ -0,0 +1,27 @@
+var name = prompt('Hello! Enter your name here please');
+var time = prompt ('What time is right now "even number"')
+if (time > 18) {
+    alert('Good evening ' + name);}
+    else if (time > 12) {
+    alert('Good afternoon ' + name);}
+    else if (time > O) {
+    alert('Good morning ' + name);}
+    else {
+     alert('Welcome ' + name);}
+    
+
+
+var profession = prompt ('What is your profession');
+var education = prompt ('what is your education?');
+var experience = prompt ('Put your experience history here');
+var phone = prompt ('please enter your phone number');
+var email = prompt ( 'and your email');
+
+document.write ("Education = " )
+
+document.write (  education ) 
+
+Document.write('Experience = ' )
+
+document.write ( 'phone = ')
+document.write (phone)
