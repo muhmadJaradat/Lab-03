5a77bc76d18773f0228e80337f1eafedbc5f1464
muhmadjaradat <you@example.com>
2021-03-08 15:31:26 +0200

    Updating the file

--- JAVA.js
+++ JAVA.js
@@ -10,27 +10,57 @@ if (time > 18) {
      alert('Welcome ' + name);}
     
 
-
-var education = prompt ('what is your education?');
-var experience = prompt ('Put your experience history here');
-var phone = prompt ('please enter your phone number');
-var email = prompt ( 'and your email');
-
-document.write ("Education = " )
-
-document.write (  education ) 
+function YourEducation() {var education = prompt ('what is your education?');
+document.write ("Education = " );
+document.write (  education ) ;
 document.write("<br>");
-
-document.write('Experience = ' )
-document.write(experience)
 document.write("<br>");
+document.write("<br>");
+}
+YourEducation();
+
+function YourExperience() {var experience = prompt ('Put your experience history here');
+document.write('Experience = ' );
+document.write(experience);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+    
+}
+YourExperience();
+
+function YourPhone() {var phone = prompt ('please enter your phone number');
 document.write ( 'phone = ')
-document.write (phone)
+document.write (phone);
 document.write("<br>");
-document.write('Email = ')
-document.write (email)
 document.write("<br>");
-var rate = prompt ("Please rate us (out of 5)")
-while (!(rate >=0 && rate <=5)) {var rate = prompt ("Please rate us (out of 5)") }
+document.write("<br>");
+}
+YourPhone();
+
+function YourEmail() {var email = prompt ( 'and your email');
+document.write('Email = ');
+document.write (email);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+}
+YourEmail();
+function Rating() {var rate = prompt ("Please rate us (out of 5)");
+while (!(rate >=0 && rate <=5)) {var rate = prompt ("Please rate us (out of 5)") };
 for (var i=0; i < rate; i++) {document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Golden_star.svg/1200px-Golden_star.svg.png" width="200" height="150">')}
+
+    
+}
+Rating();
+
+
+
+
+
+
+
+
+
+
 
